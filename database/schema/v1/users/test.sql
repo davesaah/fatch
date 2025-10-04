cad6067f3d1d65679485a0ca087c002a265f1f82
David <davidreturns08@laelfamily.org>
2025-10-04 10:39:12 +0000

    use only functions for encapsulation of queries

--- database/schema/v1/users/test.sql
+++ database/schema/v1/users/test.sql
@@ -1,16 +1,17 @@
 -- Active: 1758835250687@@127.0.0.1@5432@local@fatch
 -- Flow 1: Create a user
-CALL fatch.create_user (
-    'davesaah',
-    'davesaah@gmail.com',
-    'password123'
-);
 
-CALL fatch.create_user (
-    'davesaah',
-    'davesaah@gmail.com',
-    'password12345'
-);
+SELECT fatch.create_user (
+        'someone', 'someone@gmail.com', 'password123'
+    );
+
+SELECT fatch.create_user (
+        'davesaah', 'davesaah@gmail.com', 'password123'
+    );
+
+SELECT fatch.create_user (
+        'davesaah', 'davesaah@gmail.com', 'password12345'
+    );
 -- Flow 2: Verify password
 SELECT fatch.verify_password ('davesaah', '', 'password123');
 
@@ -29,20 +30,14 @@ SELECT fatch.verify_password (
 SELECT fatch.verify_password ( 'davesaah', '', 'newpassword456' );
 
 -- Flow 3: Change password
-CALL fatch.change_password (
-    '360ef0fa-75a0-4fc3-b842-00fb4725b353',
-    'passwod123',
-    'newpassword456'
-);
+SELECT fatch.change_password (
+        '360ef0fa-75a0-4fc3-b842-00fb4725b353', 'passwod123', 'newpassword456'
+    );
 
-CALL fatch.change_password (
-    '330ef0fa-75a0-4fc3-b842-00fb4725b353',
-    'passwod123',
-    'newpassword456'
-);
+SELECT fatch.change_password (
+        '330ef0fa-75a0-4fc3-b842-00fb4725b353', 'passwod123', 'newpassword456'
+    );
 
-CALL fatch.change_password (
-    '360ef0fa-75a0-4fc3-b842-00fb4725b353',
-    'password123',
-    'newpassword456'
-);
+SELECT fatch.change_password (
+        '360ef0fa-75a0-4fc3-b842-00fb4725b353', 'password123', 'newpassword456'
+    );
