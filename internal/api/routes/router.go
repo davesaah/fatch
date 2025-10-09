3a16fda9f10bf67ba41baa29f06ab1ef93f4a862
David <davidreturns08@laelfamily.org>
2025-10-09 20:14:06 +0000

    feat: add some useful endpoints

--- internal/api/routes/router.go
+++ internal/api/routes/router.go
@@ -1,10 +1,15 @@
 package routes
 
 import (
+	"time"
+
 	"github.com/davidreturns08/fatch/internal/api/handlers"
 	"github.com/davidreturns08/fatch/internal/api/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/go-chi/httprate"
 
+	chiMiddleware "github.com/go-chi/chi/v5/middleware"
 	httpSwagger "github.com/swaggo/http-swagger"
 )
 
@@ -13,10 +18,32 @@ func SetupV1Routes() *chi.Mux {
 	r := chi.NewRouter()
 
 	// setup middlewares
+	r.Use(chiMiddleware.AllowContentType("application/json"))
+
+	// update later
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	r.Use(chiMiddleware.Recoverer)
 	r.Use(middleware.JSONLogger)
+
+	// limit to 100 requests per minute for each unique IP
+	// look into expanding the rate limiter function to sensitive endpoints
+	r.Use(httprate.LimitByIP(100, 1*time.Minute))
 
 	// API ROUTES
 	r.Route("/v1", func(r chi.Router) {
+		// profiler
+		r.Mount("/debug", chiMiddleware.Profiler())
+
 		// Swagger documentation
 		r.Get("/swagger/*", httpSwagger.WrapHandler)
 		r.Get("/swagger/doc.json", handlers.ServeDocFile)
