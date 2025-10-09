14e5976fb1feeda6cd55f00fabe30af0ad57b5fe
David <davidreturns08@laelfamily.org>
2025-10-09 07:56:04 +0000

    feat: switch to chi router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,17 +4,17 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/davidreturns08/fatch/internal/api"
+	"github.com/davidreturns08/fatch/internal/api/routes"
 )
 
+// @title Fatch API
+// @version 1.0
+// @description Track your money; fetch insights on spending; budget effectively.
+// @host localhost:5000
+// @BasePath /api/v1
 func main() {
-	mux := api.SetupRoutes()
-
-	server := &http.Server{
-		Addr:    ":5000",
-		Handler: mux,
-	}
-
-	log.Printf("Starting server at localhost%s\n", server.Addr)
-	log.Fatal(server.ListenAndServe())
+	mux := routes.SetupV1Routes()
+	log.Println("API server started on http://localhost:500/api/v1")
+	log.Println("API docs available at http://localhost:5000/swagger/index.html")
+	http.ListenAndServe(":5000", mux)
 }
---
+++ internal/api/routes/router.go
@@ -0,0 +1,39 @@
+package routes
+
+import (
+	"github.com/davidreturns08/fatch/internal/api/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	httpSwagger "github.com/swaggo/http-swagger"
+)
+
+// setupV1Routes sets up the routes for the v1 API.
+func SetupV1Routes() *chi.Mux {
+	r := chi.NewRouter()
+
+	// setup middlewares
+	r.Use(middleware.Logger)
+
+	// Swagger documentation
+	r.Get("/swagger/*", httpSwagger.WrapHandler)
+
+	// Health check endpoint
+	// r.Get("/health", handlers.HealthCheck)
+
+	// API ROUTES
+	r.Route("/api/v1", func(r chi.Router) {
+		// USER ROUTES
+		r.Route("/users", func(r chi.Router) {
+			r.Post("/", handlers.CreateUser)
+			r.Patch("/passwd", handlers.ChangePassword)
+		})
+
+		// AUTH ROUTES
+		r.Route("/auth", func(r chi.Router) {
+			r.Post("/verify", handlers.VerifyPassword)
+		})
+	})
+
+	return r
+}
