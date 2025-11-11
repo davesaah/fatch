b43958a07298a7a4127047b2cfc8a57cc8b53e59
David Saah <dave@davesaah.com>
2025-11-11 09:17:42 +0000

    chore: use localhost

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,15 +11,15 @@ import (
 // @title Fatch API
 // @version 1.0
 // @description Track your money; fetch insights on spending; budget effectively.
-// @host api.fatch.laelfamily.org:8000
+// @host localhost:8000
 // @BasePath /v1
 func main() {
 	mux := routes.SetupV1Routes()
 
-	log.Println("API server started on http://api.fatch.laelfamily.org:8000/v1")
+	log.Println("API server started on http://localhost:8000/v1")
 	if os.Getenv("ENVIRONMENT") == "dev" {
-		log.Println("API docs available at http://api.fatch.laelfamily.org:8000/v1/swagger/index.html")
-		log.Println("API profiler available at http://api.fatch.laelfamily.org:8000/v1/debug")
+		log.Println("API docs available at http://localhost:8000/v1/swagger/index.html")
+		log.Println("API profiler available at http://localhost:8000/v1/debug")
 	}
 
 	err := http.ListenAndServe(":8000", mux)
